cccec611cabd0e0f38c862f91146c39690f932b2
spongebobbryant24 <jamshaid.mirpour23@gmail.com>
2026-02-15 21:41:20 -0500

    add w04

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -1,6 +1,21 @@
 # Sample Coding Questions 01 Week 01
 # Jamshaid Mirpour
 
-# Question 2: Defining an array (list)
+# Defining Variables
 numbers = [1, 4, 7, 9]
 
+# Order of Operations
+a, b, c, d = 1, 2, 3, 4
+
+# Fully-bracketed version of:
+# e = a - b ** c // d + a % c
+e = (a - ((b ** c) // d)) + (a % c)
+
+# Formatting (1 line, no hard-coded extra zeros)
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Common Functions
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print("Now showing the shop items filtered by age: {}".format(userAge))
